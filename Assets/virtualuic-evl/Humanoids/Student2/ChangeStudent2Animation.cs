977b8266c0abc6802e449b81cb7ff41f9d2844c0
Harish Venkataraman <harishvenkat238@gmail.com>
2020-11-27 02:42:28 -0600

    Added assets for small and large objects + humanoids

---
+++ Assets/virtualuic-evl/Humanoids/Student2/ChangeStudent2Animation.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeStudent2Animation : MonoBehaviour
+{
+    public Animator humanoidAnimator; 
+    public AudioSource humanoidAudioSource;
+
+    public GameObject user;
+    public float minimumDistance;
+    bool isClose; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        isClose = false;
+        humanoidAnimator.GetComponent<Animator>();
+        humanoidAudioSource.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(gameObject.transform.localPosition, user.transform.localPosition) <= (minimumDistance - 0.2f) && !isClose) {
+            isClose = true;
+            humanoidAnimator.SetTrigger("AngryTrigger");
+            humanoidAudioSource.Play();
+        }
+        else if (Vector3.Distance(gameObject.transform.localPosition, user.transform.localPosition) > (minimumDistance + 0.2f)) {
+            isClose = false;
+        }
+    }
+}
---
+++ Assets/virtualuic-evl/Humanoids/Student3/ChangeStudent3Animation.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeStudent3Animation : MonoBehaviour
+{
+    public Animator humanoidAnimator; 
+    public AudioSource humanoidAudioSource;
+    public GameObject user;
+    public float minimumDistance;
+    bool isClose; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        isClose = false;
+        humanoidAnimator.GetComponent<Animator>();
+        humanoidAudioSource.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(gameObject.transform.localPosition, user.transform.localPosition) <= (minimumDistance - 0.2f) && !isClose) {
+            isClose = true;
+            humanoidAnimator.SetTrigger("VictoryTrigger");
+            humanoidAudioSource.Play();
+        }
+        else if (Vector3.Distance(gameObject.transform.localPosition, user.transform.localPosition) > (minimumDistance + 0.2f)) {
+            isClose = false;
+        }
+    }
+}
