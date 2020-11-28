8c6122763a7c54b5a1c8c68b8690b2a0a4b0aa92
Harish Venkataraman <harishvenkat238@gmail.com>
2020-11-27 20:41:21 -0600

    Add objects

---
+++ Assets/virtualuic-evl/AssetStore/LowPolyEarth/EarthSpinScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EarthSpinScript : MonoBehaviour {
+    public float speed = 10f;
+
+    bool canRotate = true;
+
+    public void StopRotation() {
+        canRotate = false;
+    }
+
+    public void StartRotation() {
+        canRotate = true;
+    }
+
+    void Update() {
+        if (canRotate) {
+            transform.Rotate(Vector3.up, speed * Time.deltaTime, Space.World);
+        }
+    }
+}
