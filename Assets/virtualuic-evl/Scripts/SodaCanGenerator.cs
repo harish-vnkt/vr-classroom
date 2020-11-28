ffd583602ccb44bde5445ed145afec831aadcbe3
Harish Venkataraman <harishvenkat238@gmail.com>
2020-11-28 00:19:19 -0600

    Added soda can generator and colliders

---
+++ Assets/virtualuic-evl/Scripts/SodaCanGenerator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SodaCanGenerator : MonoBehaviour
+{   
+    public GameObject sodaCanPrefab;
+    public GameObject generateObject;
+    // Start is called before the first frame update
+    public void Generate() {
+        Instantiate(sodaCanPrefab, generateObject.transform.position, Quaternion.identity);
+    }
+}
