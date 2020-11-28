7cef64a1e00ff495843928118271d4f20970c248
Harish Venkataraman <harishvenkat238@gmail.com>
2020-11-27 20:40:26 -0600

    Add model of me

--- Assets/virtualuic-evl/Humanoids/ChangeStudentAnimation.cs
+++ Assets/virtualuic-evl/Humanoids/ChangeStudentAnimation.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ChangeStudent3Animation : MonoBehaviour
+public class ChangeStudentAnimation : MonoBehaviour
 {
     public Animator humanoidAnimator; 
     public AudioSource humanoidAudioSource;
     public GameObject user;
     public float minimumDistance;
-    bool isClose; 
+    public string triggerName;
+    bool isClose;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,7 +23,7 @@ public class ChangeStudent3Animation : MonoBehaviour
     {
         if (Vector3.Distance(gameObject.transform.localPosition, user.transform.localPosition) <= (minimumDistance - 0.2f) && !isClose) {
             isClose = true;
-            humanoidAnimator.SetTrigger("VictoryTrigger");
+            humanoidAnimator.SetTrigger(triggerName);
             humanoidAudioSource.Play();
         }
         else if (Vector3.Distance(gameObject.transform.localPosition, user.transform.localPosition) > (minimumDistance + 0.2f)) {
